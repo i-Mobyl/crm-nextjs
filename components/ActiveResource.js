55cc12a28ace334f22c0679841428f88acc2832d
i-Mobyl <57107385+i-Mobyl@users.noreply.github.com>
2022-01-19 17:29:15 -0500

    Fixed route in ActiveResource.js to use env

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -14,7 +14,7 @@ const ActiveResource = () => {
   // Run this once when loaded
   useEffect(() => {
     const fetchActiveResource = async () => {
-      const res = await axios.get(`${process.env.API_URL}/activeresource`)
+      const res = await axios.get("api/activeresource");
       const resource = res.data;
       const timeToFinish = parseInt(resource.timeToFinish, 10);
       
